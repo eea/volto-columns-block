d57e2c1c3c84608af2bbd122cb14ce58b2f120fe
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-21 12:40:56 +0300

    Use RenderBlocks component

--- src/ColumnsBlock/ColumnsBlockView.jsx
+++ src/ColumnsBlock/ColumnsBlockView.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { renderBlocks } from '@plone/volto/helpers';
+// import { renderBlocks } from '@plone/volto/helpers';
 import { getColumns, empty } from './utils';
+import { RenderBlocks } from '@plone/volto/components';
 
 const ColumnsBlockView = (props) => {
   const { coldata = empty(), block_title } = props.data;
@@ -14,7 +15,7 @@ const ColumnsBlockView = (props) => {
           return (
             <Grid.Column className="demo-column" key={id}>
               <h4>{`Column ${index}`}</h4>
-              {renderBlocks(column, props)}
+              <RenderBlocks {...props} content={column} />
             </Grid.Column>
           );
         })}
