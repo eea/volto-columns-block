b905b9234a3e9ab8231f79405f5604bbb0b50e87
Silviu Bogan <silviubogan@gmail.com>
2020-10-09 13:23:52 +0300

    Delete button doesn't appear for the only block in column

--- src/ColumnsBlock/EditBlockWrapper.jsx
+++ src/ColumnsBlock/EditBlockWrapper.jsx
@@ -12,6 +12,8 @@ import dragSVG from '@plone/volto/icons/drag.svg';
 import addSVG from '@plone/volto/icons/circle-plus.svg';
 import trashSVG from '@plone/volto/icons/delete.svg';
 
+import { getColumns } from './utils';
+
 const messages = defineMessages({
   unknownBlock: {
     id: 'Unknown Block',
@@ -74,6 +76,8 @@ class EditBlockWrapper extends React.Component {
     // const visible = selected && blockHasValue(block) && !block.fixed;
     // visibility: visible ? 'visible' : 'hidden',
 
+    const showDeleteButton = getColumns(blockProps.properties).length > 1;
+
     const required = isBoolean(data.required)
       ? data.required
       : includes(blocks.requiredBlocks, type);
@@ -125,7 +129,7 @@ class EditBlockWrapper extends React.Component {
                   <Icon name={addSVG} className="" size="19px" />
                 </Button>
               )}
-              {!required && (
+              {!required && showDeleteButton && (
                 <Button
                   icon
                   basic
