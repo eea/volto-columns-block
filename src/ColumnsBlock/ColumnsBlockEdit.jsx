0c54b745979db06ecc6cabf76976548fd347faa5
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-18 18:22:05 +0300

    Add BBB message

--- src/ColumnsBlock/ColumnsBlockEdit.jsx
+++ src/ColumnsBlock/ColumnsBlockEdit.jsx
@@ -208,6 +208,7 @@ class ColumnsBlockEdit extends React.Component {
     // }}
     return (
       <div role="presentation" className="columns-block">
+        {data.coldata ? 'old style columns block, safe to remove it' : ''}
         {Object.keys(data).length === 1 ? (
           <ColumnVariations
             variants={variants}
