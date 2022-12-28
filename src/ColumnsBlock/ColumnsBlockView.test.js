1a5b9afeec15d998b24e3d9bedaa9c9044a54f9b
Alin Voinea <contact@avoinea.com>
2022-12-28 15:46:06 +0200

    test(eslint): Fix lint

--- src/ColumnsBlock/ColumnsBlockView.test.js
+++ src/ColumnsBlock/ColumnsBlockView.test.js
@@ -55,14 +55,14 @@ const TextView = ({ data }) => {
   return data.text;
 };
 
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
   useLocation: () => ({
-    pathname: "/",
-    hash: "",
-    search: "",
-    state: undefined
-  })
+    pathname: '/',
+    hash: '',
+    search: '',
+    state: undefined,
+  }),
 }));
 
 test('renders 2 columns', async () => {
