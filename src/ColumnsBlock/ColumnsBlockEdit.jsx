8214137419e3ef9a6408cc59526862a552a9f4d1
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-08-05 18:55:10 +0300

    Update ColumnsBlockEdit.jsx to pass props to blocks form

--- src/ColumnsBlock/ColumnsBlockEdit.jsx
+++ src/ColumnsBlock/ColumnsBlockEdit.jsx
@@ -389,6 +389,7 @@ class ColumnsBlockEdit extends React.Component {
                   {...getStyle(data?.data?.blocks?.[colId]?.settings || {})}
                 >
                   <BlocksForm
+                    {...this.props}
                     key={colId}
                     title={data?.placeholder}
                     description={data?.instructions?.data}
