275270f44b4e269a5e42fbec9d8f9a3b1ab832cc
Alin Voinea <contact@avoinea.com>
2026-08-12 16:45:44 +0300

    Volto 19 compatibility: use @plone/volto/babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,1 @@
-module.exports = function (api) {
-  api.cache(true);
-  const presets = ['razzle'];
-  const plugins = [
-    '@babel/plugin-proposal-export-default-from',
-    [
-      'react-intl', // React Intl extractor, required for the whole i18n infrastructure to work
-      {
-        messagesDir: './build/messages/',
-      },
-    ],
-  ];
-
-  return {
-    plugins,
-    presets,
-  };
-};
+module.exports = require('@plone/volto/babel');
